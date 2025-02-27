ce398980026311f361e01c8f78ff29256e46fcef
Marcos Rocha <marcosrocha0096@gmail.com>
2025-02-27 18:11:38 -0300

    Chamadas State Holder -> Reutilização mais simples -> Reutilização mais refinada e com mais controle e flexibilidade.
    Conceitos de stateful & stateless

--- app/src/main/java/com/example/aludelivery/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/aludelivery/ui/activities/MainActivity.kt
@@ -13,17 +13,9 @@ import androidx.compose.material.Surface
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.aludelivery.dao.ProductDao
-import com.example.aludelivery.model.Product
-import com.example.aludelivery.sampledata.sampleCandies
-import com.example.aludelivery.sampledata.sampleDrinks
-import com.example.aludelivery.sampledata.sampleProducts
 import com.example.aludelivery.sampledata.sampleSections
 import com.example.aludelivery.ui.screens.HomeScreen
 import com.example.aludelivery.ui.screens.HomeScreenContent
